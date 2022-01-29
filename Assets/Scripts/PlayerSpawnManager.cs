d5f960550ba6c4c42c205e4995b1426ecd4f0900
Mason Beale <mbeale@cedarville.edu>
2022-01-28 23:01:41 -0500

    added capsule

--- Assets/Scripts/PlayerSpawnManager.cs
+++ Assets/Scripts/PlayerSpawnManager.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerSpawnManager : MonoBehaviour
 {
-    void OnPlayerJoined()
+    [SerializeField] private GameObject[] spawnLocations = null;
+
+    public void OnPlayerJoined(PlayerInput playerInput)
     {
-        Debug.Log("player joined");
+        Debug.Log("player joined with ID: " + playerInput.playerIndex);
+
+        playerInput.gameObject.GetComponent<PlayerDetails>().setPlayerID(playerInput.playerIndex + 1);
+
+        playerInput.gameObject.GetComponent<PlayerDetails>().setPlayerStart(spawnLocations[playerInput.playerIndex].transform.position);
     }
 }
